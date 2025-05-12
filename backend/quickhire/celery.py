8a61ac32f500202daae4b3f4f18639f2804905dd
nearlys40 <nearlysza@gmail.com>
2025-05-12 16:00:24 +0700

    Seperate settings.py to base.py + dev.py + prod.py, Update value for DJANGO_SETTINGS_MODULE for production

--- backend/quickhire/celery.py
+++ backend/quickhire/celery.py
@@ -1,7 +1,7 @@
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quickhire.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quickhire.settings.dev')
 
 app = Celery('quickhire')
 app.config_from_object('django.conf:settings', namespace='CELERY')
