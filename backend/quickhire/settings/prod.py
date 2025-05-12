e32c3297ea2b617b2b200a6b0b792d501f845a50
nearlys40 <nearlysza@gmail.com>
2025-05-12 17:34:49 +0700

    Update settings/prod.py value

--- backend/quickhire/settings/prod.py
+++ backend/quickhire/settings/prod.py
@@ -1,3 +1,19 @@
 from .base import *
+import os
+import dj_database_url
 
 DEBUG = False
+
+REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379")
+
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [REDIS_URL],
+        },
+    },
+}
+
+CELERY_BROKER_URL = REDIS_URL
+CELERY_RESULT_BACKEND = REDIS_URL
