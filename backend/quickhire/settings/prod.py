bdfe1de7d7bc7a813cf33777d4acef1eb0b21ee0
nearlys40 <nearlysza@gmail.com>
2025-05-13 21:55:07 +0700

    Add SECURE_PROXY_SSL_HEADER to fix too many redirects

--- backend/quickhire/settings/prod.py
+++ backend/quickhire/settings/prod.py
@@ -36,6 +36,8 @@ CORS_ALLOWED_ORIGINS = [
     "https://quickhire-frontend-production.up.railway.app",     
 ]
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
