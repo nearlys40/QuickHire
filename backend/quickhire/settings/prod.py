feb81435f6fd38803d5d892a9a137c26edd431ca
nearlys40 <nearlysza@gmail.com>
2025-05-13 19:35:15 +0700

    Update security for backend

--- backend/quickhire/settings/prod.py
+++ backend/quickhire/settings/prod.py
@@ -26,7 +26,18 @@ CHANNEL_LAYERS = {
 CELERY_BROKER_URL = REDIS_URL
 CELERY_RESULT_BACKEND = REDIS_URL
 
+ALLOWED_HOSTS = [
+    "quickhire-production.up.railway.app",
+    "quickhire-frontend-production.up.railway.app"
+]
+
 # CORS
 CORS_ALLOWED_ORIGINS = [
     "https://quickhire-frontend-production.up.railway.app",     
 ]
+
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
